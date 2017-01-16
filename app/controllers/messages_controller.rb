2697c3bc4aee0fa2d817b00ebb76c6efd348438b
King <kingsleywang2013@gmail.com>
2017-01-16 17:09:22 +1100

    Implemented metion notification

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,10 @@ class MessagesController < ApplicationController
     if message.save
       ActionCable.server.broadcast 'room_channel',
                                    message: render_message(message)
-      head :ok
+      message.mentions.each do |mention|
+        ActionCable.server.broadcast "room_channel_user_#{mention.id}",
+                                     mention: true
+      end
     end
   end
 
